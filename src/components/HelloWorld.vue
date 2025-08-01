23cd10986503e3703602c7e196791ee8771163d4
gsm@ <gsm@example.com>
2025-08-01 17:31:46 +0530

    Updated vue file

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,6 +1,9 @@
 <template>
   <div class="hello">
+  <h1> Welcome to Git page page - Gajeder</h1>
     <h1>{{ msg }}</h1>
+    <BR>
+    
     <p>
       For a guide and recipes on how to configure / customize this project,<br>
       check out the
