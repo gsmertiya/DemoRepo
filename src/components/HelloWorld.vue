a9ed9b3d6483ffb95de2ea11f5d6fff9502d4c53
gsmertiya <45427274+gsmertiya@users.noreply.github.com>
2025-08-02 11:00:44 +0530

    Update HelloWorld.vue

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,6 +1,9 @@
 <template>
   <div class="hello">
-  <h1> Welcome to Git page page - Gajeder</h1>
+  <h1> Welcome to Git page page - Gajender</h1>
+    <h1>{{ msg }}</h1>
+    <br>
+  <h1> Welcome to Git page page - Gajender</h1>
     <h1>{{ msg }}</h1>
     <br>
     
